948dea63f541f084a4c039a05f4227e43afd70be
Dhanush Samdaria <dhanushsamdaria.m@gmail.com>
2016-12-06 18:55:12 +0530

    Splash Screen + Signup Page
    Made the splash screen and signup page.

--- app/src/main/java/com/csivit/intranet/activities/SplashActivity.java
+++ app/src/main/java/com/csivit/intranet/activities/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Observable.timer(2, TimeUnit.SECONDS)
+        Observable.timer(5, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<Long>() {
